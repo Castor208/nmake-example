a40c7167328a69ee1fa2aa97cb983ba5e48830af
mitsuji <mitsuji>
2016-03-11 20:28:49 +0900

    1st commit

---
+++ src/lib1.c
@@ -0,0 +1,8 @@
+#include "lib1.h"
+
+#include <stdio.h>
+
+void lib1_func()
+{
+  printf("lib1_func\n");
+}
---
+++ src/lib2.c
@@ -0,0 +1,8 @@
+#include "lib2.h"
+
+#include <stdio.h>
+
+void lib2_func()
+{
+  printf("lib2_func\n");
+}
